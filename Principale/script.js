7975c7172771b5fdceb4ce37c695881b7defb1d6
Emilien4444 <131696305+Emilien4444@users.noreply.github.com>
2025-03-27 11:01:04 +0100

    Delete Principale directory

--- Principale/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const hero = document.querySelector(".hero");
-    
-    setTimeout(() => {
-        hero.style.opacity = "1";
-        hero.style.transform = "translateY(0)";
-    }, 500);
-});
