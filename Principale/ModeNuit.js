1424d769485333d9d37a6b0639d6a0838927b3a6
Emilien4444 <131696305+Emilien4444@users.noreply.github.com>
2025-04-01 15:03:48 +0200

    Update ModeNuit.js

--- Principale/ModeNuit.js
+++ Principale/ModeNuit.js
@@ -1,25 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Sélectionner le bouton de bascule pour changer le thème
     const themeToggleButton = document.getElementById('theme-toggle');
     
-    // Vérifiez si l'utilisateur a déjà un mode préféré
+    // Vérifiez si l'utilisateur a déjà un mode préféré enregistré dans le localStorage
     if (localStorage.getItem('theme') === 'dark') {
+        // Si le thème préféré est 'dark', appliquez la classe dark-theme à l'élément body
         document.body.classList.add('dark-theme');
-        themeToggleButton.textContent = 'Mode Clair'; // Texte du bouton modifié
+        // Changez le texte du bouton pour indiquer l'option "Mode Clair" 
+        themeToggleButton.textContent = 'Mode Clair';
     } else {
-        themeToggleButton.textContent = 'Mode Nuit'; // Si le mode clair est sélectionné par défaut
+        // Si le mode clair est sélectionné par défaut, affichez "Mode Nuit"
+        themeToggleButton.textContent = 'Mode Nuit';
     }
 
-    // Changez le thème au clic
+    // Ajoutez un écouteur d'événements pour changer le thème lorsque l'utilisateur clique sur le bouton
     themeToggleButton.addEventListener('click', function () {
-        // Basculez la classe dark-theme
+        // Basculez la classe dark-theme sur le body (ajoute ou supprime)
         document.body.classList.toggle('dark-theme');
         
-        // Mémorisez la préférence de l'utilisateur dans localStorage
+        // Mémorisez la préférence de l'utilisateur dans le localStorage
         if (document.body.classList.contains('dark-theme')) {
+            // Si le mode sombre est activé, enregistrez 'dark' dans le localStorage
             localStorage.setItem('theme', 'dark');
+            // Modifiez le texte du bouton pour indiquer l'option "Mode Clair"
             themeToggleButton.textContent = "Mode Clair";
         } else {
+            // Si le mode sombre est désactivé, enregistrez 'light' dans le localStorage
             localStorage.setItem('theme', 'light');
+            // Modifiez le texte du bouton pour indiquer l'option "Mode Nuit"
             themeToggleButton.textContent = 'Mode Nuit';
         }
     });
